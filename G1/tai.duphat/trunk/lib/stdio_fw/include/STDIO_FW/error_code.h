3c5163af30f02c9641c0efccee4086aaf8ff4574
vquanghuy@gmail.com <vquanghuy@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-09-27 04:03:04 +0000

    + init framework for trainee

---
+++ G1/tai.duphat/trunk/lib/stdio_fw/include/STDIO_FW/error_code.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace stdio_fw
+{
+	enum ErrorCode
+	{
+		ERR_NO_ERROR = 0,
+		ERR_INVALID_VERTEX_SHADER,
+		ERR_INVALID_FRAGMENT_SHADER,
+		ERR_INVALID_PROGRAM,
+		ERR_INVALID_OPERATION
+	};
+}
