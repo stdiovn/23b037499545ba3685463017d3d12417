074c2caf355ef10d88d5471cb8cd1bd44c96198e
uitdptai@gmail.com <uitdptai@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-10-01 07:59:20 +0000

    Bài tập vẽ hình di chuyển

--- branches/game_programming/G1/tai.duphat/trunk/source/Game.cpp
+++ branches/game_programming/G1/tai.duphat/trunk/source/Game.cpp
@@ -2,8 +2,41 @@
 
 #include "STDIO_FW\Core\Application.h"
 #include "STDIO_FW\Video\Graphics.h"
-
 #include "Game.h"
+#include <stdlib.h>
+#include <ctime>
+
+
+#define FPS 30
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+#define RED 0xFF0000FF
+#define YELLOW 0xFFFF00FF
+#define PURPLE 0x7030A0FF
+
+
+struct Vector2D
+{
+	int X;
+	int Y;
+
+	Vector2D(int X = 0, int Y = 0)
+	{
+		this->X = X;
+		this->Y = Y;
+	}
+
+	Vector2D operator +(Vector2D x)
+	{
+		return Vector2D(X + x.X, Y + x.Y);
+	}
+};
+
+Vector2D positionRect(100, 100), veclocity(1, 1);
+Vector2D positionRect2(50, 550), veclocity2(2,0);
+int color = RED;
+
 
 Game::Game()
 {
@@ -24,15 +57,63 @@ ErrorCode Game::Init(int screenW, int screenH, const char* title)
 
 void Game::Update(float deltaTime)
 {
+	float CurrenTime;
+	float StartTime = GetTickCount();
+	do
+	{
+		CurrenTime = GetTickCount();
+		CurrenTime = CurrenTime - StartTime + deltaTime;
+	} while (CurrenTime < (float)1000 / FPS);
+	
 
+	// hinh vuong
+	positionRect = positionRect + veclocity;
+
+	if (positionRect.X + 100 > SCREEN_WIDTH || positionRect.X < 0)
+	{
+		veclocity.X = -veclocity.X;
+		color = rand() % 3 + 1;
+	}
+	else 
+	if (positionRect.Y + 100 > SCREEN_HEIGHT || positionRect.Y < 0)
+	{
+		veclocity.Y = -veclocity.Y;
+		color = rand() % 3 + 1;
+	}
+
+	// hinh chu nhat
+	positionRect2.X += veclocity2.X;
+
+	if (positionRect2.X + 200 > SCREEN_WIDTH || positionRect2.X < 0) 
+		veclocity2.X = -veclocity2.X;
+
+
+	switch (color)
+	{
+	case 1:
+		color = RED;
+		break;
+	case 2:
+		color = PURPLE;
+		break;
+	case 3:
+		color = YELLOW;
+		break;
+	}
 }
+
 
 void Game::Render(Graphics* g)
 {
 	g->cleanScreen();
-	g->setColor(0x00FF00FF);
-	g->drawRect(50, 50, 200, 200);
+	g->setColor(color);
+
+	g->drawRect(positionRect.X, positionRect.Y, 100, 100);
+	g->fillRect(positionRect.X, positionRect.Y, 100, 100);
+
+	g->drawRect(positionRect2.X, positionRect2.Y, 200, 50);
 }
+
 
 void Game::Exit()
 {
@@ -44,8 +125,10 @@ void Game::Exit()
 /////////////////////////////////////
 void main()
 {
+	srand(time(NULL));
 	Game g;
-	g.Init(800, 600, "Game");
+	g.Init(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
+
 
 	g.Run();
 }
