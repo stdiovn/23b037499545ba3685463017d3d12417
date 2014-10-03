0c0e1f4eb9b377dd206f6bbc0392debc5e7dc0f5
amyle.215@gmail.com <amyle.215@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-10-03 16:44:26 +0000

    + Update 1: still bug

--- branches/game_programming/G1/amy.le/trunk/source/Game.cpp
+++ branches/game_programming/G1/amy.le/trunk/source/Game.cpp
@@ -4,7 +4,14 @@
 #include "STDIO_FW\Video\Graphics.h"
 
 #include "Game.h"
+#include <time.h>
+#include <Windows.h>
 
+#define SRCWidth 800
+#define SRCHeight 600
+#define SqrWidth 50
+#define RectWidth 200
+#define  RectHeight 30
 Game::Game()
 {
 
@@ -18,22 +25,133 @@ Game::~Game()
 ErrorCode Game::Init(int screenW, int screenH, const char* title)
 {
 	ErrorCode errCode = Application::Init(screenW, screenH, title);
-
+	m_SqrSpeedX = 2;
+	m_SqrSpeedY = 2;
+	m_RectSpeedX = 10;
+	m_SqrX = 0;
+	m_SqrY = 0;
+	m_RectX = 0;
+	m_RectY = 400;
+	m_SqrColor = 0x00FF00FF;
 	return errCode;
+}
+void Game::LimitFPS(int LimitFPS, float DeltaTime)
+{
+	int LimitFDT;
+	LimitFDT = 1000 / LimitFPS;
+	if ( DeltaTime < LimitFDT)
+	{
+		Sleep(LimitFDT - DeltaTime);
+	}
+}
+
+unsigned int Game::ColorRandom(int Red, int Green, int Blue, int Alpha)
+{
+	return ((Red << 24) | (Green << 16) | (Blue << 8) | Alpha);
 }
 
 void Game::Update(float deltaTime)
 {
+	LimitFPS(60, deltaTime);
 
+	//Square conllide with Screen
+	m_SqrX += m_SqrSpeedX;
+	m_SqrY += m_SqrSpeedY;
+
+	if (m_SqrX >= SRCWidth - SqrWidth) 
+	{
+		m_SqrX = SRCWidth - SqrWidth;
+		m_SqrSpeedX = -m_SqrSpeedX;
+	}
+	else if (m_SqrX <= 0)
+	{
+		m_SqrX = 0;
+		m_SqrSpeedX = -m_SqrSpeedX;
+	}
+	if (m_SqrY >= SRCHeight - SqrWidth) 
+	{
+		m_SqrY = SRCHeight - SqrWidth;
+		m_SqrSpeedY = -m_SqrSpeedY;
+	} 
+	else if (m_SqrY <= 0)
+	{
+		m_SqrY = 0;
+		m_SqrSpeedY = -m_SqrSpeedY;
+	}
+	//Square conllide with Rect
+	if ((m_SqrX + SqrWidth >= m_RectX) && (m_SqrX <= m_RectX + RectWidth))
+	{
+		if ((m_SqrY + SqrWidth > m_RectY) && (m_SqrY + SqrWidth <= m_RectY + RectHeight))
+		{
+			m_SqrY = m_RectY - SqrWidth;
+		}
+		else if ((m_SqrY < m_RectY + RectHeight) && (m_SqrY >= m_RectY))
+		{
+			m_SqrY = m_RectY + RectHeight;
+		}
+		if ((m_SqrY + SqrWidth == m_RectY) || (m_SqrY == m_RectY + RectHeight))
+		{
+			m_SqrSpeedY = -m_SqrSpeedY;
+		}
+	}
+	
+
+	if ((m_SqrY + SqrWidth >= m_RectY) && (m_SqrY <= m_RectY + RectHeight))
+	{
+		if ((m_SqrX + SqrWidth > m_RectX) && (m_SqrX <= m_RectX ))
+		{
+			m_SqrX = m_RectX - SqrWidth;
+		}
+		else if ((m_SqrX < m_RectX + RectWidth) && (m_SqrX > m_RectX + RectWidth - SqrWidth))
+		{
+			m_SqrX = m_RectX + RectWidth;
+		}
+		if ((m_SqrX + SqrWidth == m_RectX) || (m_SqrX == m_RectX + RectWidth))
+		{
+			m_SqrSpeedX = -m_SqrSpeedX;
+		}
+	}
+	
+	//Press Key
+	if ((m_RectX <SRCHeight) && (GetAsyncKeyState(VK_RIGHT)))
+	{
+		m_RectX += m_RectSpeedX;
+		if (m_RectSpeedX < 0)
+		{
+			m_RectSpeedX = -m_RectSpeedX;
+		}
+	}
+	else if (m_RectX > 0 && GetAsyncKeyState(VK_LEFT))
+	{
+		m_RectX += m_RectSpeedX;
+		if (m_RectSpeedX > 0)
+		{
+			m_RectSpeedX = -m_RectSpeedX;
+		}
+	}
+	//Game Over
+	if (m_SqrY +SqrWidth >= SRCHeight)
+	{
+		int Red = rand() % 256;
+		int Green = rand() % 256;
+		int Blue = rand() % 256;
+		m_SqrColor = ColorRandom(Red, Green, Blue, 0);
+	}
 }
 
-void Game::Render(Graphics* g)
+void Game::Render(Graphics* g/*, Block* b, RectControl* r*/)
 {
 	g->cleanScreen();
-	g->setColor(0x00FF00FF);
-	g->drawRect(50, 50, 200, 200);
+	/*b->InitBlock();
+	b->RenderBlock(g);
+	r->InitRectControl();
+	r->RenderRectControl(g);*/
+	g->setColor(m_SqrColor);
+	g->fillRect(m_SqrX, m_SqrY, SqrWidth, SqrWidth);
+	g->setColor(0xFF0000FF);
+	g->drawRect(m_SqrX, m_SqrY, SqrWidth, SqrWidth);
+	g->fillRect(m_RectX, m_RectY, RectWidth, RectHeight);
 }
-
 void Game::Exit()
 {
 
@@ -45,7 +163,9 @@ void Game::Exit()
 void main()
 {
 	Game g;
-	g.Init(800, 600, "Game");
-
+	
+	g.Init(SRCWidth, SRCHeight, "Game");
 	g.Run();
+
+	
 }
--- branches/game_programming/G1/amy.le/trunk/source/Game.h
+++ branches/game_programming/G1/amy.le/trunk/source/Game.h
@@ -1,15 +1,27 @@
 #pragma once
+//#include "Block.h"
+//#include "RectControl.h"
 
 using namespace stdio_fw;
 class Game : public Application
 {
+private:
+	int m_SqrSpeedX;
+	int m_SqrSpeedY;
+	int m_RectSpeedX;
+	int m_SqrX;
+	int m_SqrY;
+	int m_RectX;
+	int m_RectY;
+	unsigned int m_SqrColor;
 public:
 	Game();
 	virtual ~Game();
-
+	virtual void		LimitFPS(int Limit, float DeltaTime);
+	virtual unsigned int ColorRandom(int Red, int Green, int Blue, int Alpha);
 	virtual ErrorCode	Init(int screenW, int screenH, const char* title);
 	virtual void		Update(float deltaTime);
 
-	virtual void		Render(Graphics* g);
+	virtual void		Render(Graphics* g/*, Block* b, RectControl* r*/);
 	virtual void		Exit();
 };
