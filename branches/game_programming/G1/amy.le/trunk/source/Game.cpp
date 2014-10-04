a1d6088b2164e9b8c2ee5a9149315e377898217e
amyle.215@gmail.com <amyle.215@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-10-04 22:42:47 +0000

    + It had image but still bug

--- branches/game_programming/G1/amy.le/trunk/source/Game.cpp
+++ branches/game_programming/G1/amy.le/trunk/source/Game.cpp
@@ -1,17 +1,16 @@
 #include "stdafx.h"
-
 #include "STDIO_FW\Core\Application.h"
 #include "STDIO_FW\Video\Graphics.h"
+#include "STDIO_FW\Video\Image.h"
 
 #include "Game.h"
+
 #include <time.h>
 #include <Windows.h>
 
 #define SRCWidth 800
 #define SRCHeight 600
-#define SqrWidth 50
-#define RectWidth 200
-#define  RectHeight 30
+
 Game::Game()
 {
 
@@ -25,16 +24,16 @@ Game::~Game()
 ErrorCode Game::Init(int screenW, int screenH, const char* title)
 {
 	ErrorCode errCode = Application::Init(screenW, screenH, title);
-	m_SqrSpeedX = 2;
-	m_SqrSpeedY = 2;
-	m_RectSpeedX = 10;
-	m_SqrX = 0;
-	m_SqrY = 0;
-	m_RectX = 0;
-	m_RectY = 400;
 	m_SqrColor = 0x00FF00FF;
+	R = new Rect;
+	R->InitRect();
+	S = new Square;
+	S->InitSquare();
+	GameOverImage = new Image("GameOver.jpg");
+	ErrorCode code = GameOverImage->loadImage();
 	return errCode;
 }
+
 void Game::LimitFPS(int LimitFPS, float DeltaTime)
 {
 	int LimitFDT;
@@ -52,120 +51,50 @@ unsigned int Game::ColorRandom(int Red, int Green, int Blue, int Alpha)
 
 void Game::Update(float deltaTime)
 {
+	//Limit FPS
 	LimitFPS(60, deltaTime);
-
-	//Square conllide with Screen
-	m_SqrX += m_SqrSpeedX;
-	m_SqrY += m_SqrSpeedY;
-
-	if (m_SqrX >= SRCWidth - SqrWidth) 
-	{
-		m_SqrX = SRCWidth - SqrWidth;
-		m_SqrSpeedX = -m_SqrSpeedX;
-	}
-	else if (m_SqrX <= 0)
-	{
-		m_SqrX = 0;
-		m_SqrSpeedX = -m_SqrSpeedX;
-	}
-	if (m_SqrY >= SRCHeight - SqrWidth) 
-	{
-		m_SqrY = SRCHeight - SqrWidth;
-		m_SqrSpeedY = -m_SqrSpeedY;
-	} 
-	else if (m_SqrY <= 0)
-	{
-		m_SqrY = 0;
-		m_SqrSpeedY = -m_SqrSpeedY;
-	}
-	//Square conllide with Rect
-	if ((m_SqrX + SqrWidth >= m_RectX) && (m_SqrX <= m_RectX + RectWidth))
-	{
-		if ((m_SqrY + SqrWidth > m_RectY) && (m_SqrY + SqrWidth <= m_RectY + RectHeight))
-		{
-			m_SqrY = m_RectY - SqrWidth;
-		}
-		else if ((m_SqrY < m_RectY + RectHeight) && (m_SqrY >= m_RectY))
-		{
-			m_SqrY = m_RectY + RectHeight;
-		}
-		if ((m_SqrY + SqrWidth == m_RectY) || (m_SqrY == m_RectY + RectHeight))
-		{
-			m_SqrSpeedY = -m_SqrSpeedY;
-		}
-	}
-	
-
-	if ((m_SqrY + SqrWidth >= m_RectY) && (m_SqrY <= m_RectY + RectHeight))
-	{
-		if ((m_SqrX + SqrWidth > m_RectX) && (m_SqrX <= m_RectX ))
-		{
-			m_SqrX = m_RectX - SqrWidth;
-		}
-		else if ((m_SqrX < m_RectX + RectWidth) && (m_SqrX > m_RectX + RectWidth - SqrWidth))
-		{
-			m_SqrX = m_RectX + RectWidth;
-		}
-		if ((m_SqrX + SqrWidth == m_RectX) || (m_SqrX == m_RectX + RectWidth))
-		{
-			m_SqrSpeedX = -m_SqrSpeedX;
-		}
-	}
-	
+	//transfer square
+	S->TransferSquare(R);
 	//Press Key
-	if ((m_RectX <SRCHeight) && (GetAsyncKeyState(VK_RIGHT)))
-	{
-		m_RectX += m_RectSpeedX;
-		if (m_RectSpeedX < 0)
-		{
-			m_RectSpeedX = -m_RectSpeedX;
-		}
-	}
-	else if (m_RectX > 0 && GetAsyncKeyState(VK_LEFT))
-	{
-		m_RectX += m_RectSpeedX;
-		if (m_RectSpeedX > 0)
-		{
-			m_RectSpeedX = -m_RectSpeedX;
-		}
-	}
-	//Game Over
-	if (m_SqrY +SqrWidth >= SRCHeight)
-	{
-		int Red = rand() % 256;
-		int Green = rand() % 256;
-		int Blue = rand() % 256;
-		m_SqrColor = ColorRandom(Red, Green, Blue, 0);
-	}
+	R->TransferRect(getKeyState(KEY_RIGHT), getKeyState(KEY_LEFT));
 }
 
-void Game::Render(Graphics* g/*, Block* b, RectControl* r*/)
+void Game::Render(Graphics* g)
 {
 	g->cleanScreen();
-	/*b->InitBlock();
-	b->RenderBlock(g);
-	r->InitRectControl();
-	r->RenderRectControl(g);*/
-	g->setColor(m_SqrColor);
-	g->fillRect(m_SqrX, m_SqrY, SqrWidth, SqrWidth);
-	g->setColor(0xFF0000FF);
-	g->drawRect(m_SqrX, m_SqrY, SqrWidth, SqrWidth);
-	g->fillRect(m_RectX, m_RectY, RectWidth, RectHeight);
+	//Draw square
+	g->drawImage(S->GetSquareImage(), S->GetSqrX(), S->GetSqrY());
+	//Draw rect
+	g->drawImage(R->GetRectImage(), R->GetRectX(), R->GetRectY());
+	//Draw Game Over Image 
+	if (S->GetSqrY() + SqrWidth >= SRCHeight)
+	{
+		int StarTime = clock();
+		g->cleanScreen();
+		GameOverImage->scale(3);
+		g->drawImage(GameOverImage, 0, 0);
+		int EndTime = clock();
+		if ((EndTime - StarTime) > 1000 / 5)
+		{
+			Sleep(1000 / 5 - (EndTime - StarTime));
+		}
+	}
 }
+
 void Game::Exit()
 {
-
+	S->ReleaseSquare();
+	R->ReleaseRect();
 }
 
 /////////////////////////////////////
 // Entry Point
 /////////////////////////////////////
+
 void main()
 {
 	Game g;
 	
 	g.Init(SRCWidth, SRCHeight, "Game");
 	g.Run();
-
-	
 }
--- branches/game_programming/G1/amy.le/trunk/source/Game.h
+++ branches/game_programming/G1/amy.le/trunk/source/Game.h
@@ -1,18 +1,14 @@
 #pragma once
-//#include "Block.h"
-//#include "RectControl.h"
-
+#include "Rect.h"
+#include "Square.h"
 using namespace stdio_fw;
 class Game : public Application
 {
 private:
-	int m_SqrSpeedX;
-	int m_SqrSpeedY;
-	int m_RectSpeedX;
-	int m_SqrX;
-	int m_SqrY;
-	int m_RectX;
-	int m_RectY;
+	
+	Rect* R;
+	Square* S;
+	Image* GameOverImage;
 	unsigned int m_SqrColor;
 public:
 	Game();
@@ -22,6 +18,6 @@ public:
 	virtual ErrorCode	Init(int screenW, int screenH, const char* title);
 	virtual void		Update(float deltaTime);
 
-	virtual void		Render(Graphics* g/*, Block* b, RectControl* r*/);
+	virtual void		Render(Graphics* g);
 	virtual void		Exit();
 };
