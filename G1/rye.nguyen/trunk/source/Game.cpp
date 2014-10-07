e20205a297966bad52c830727e5f02bbc707dd95
ryenguyen7411@gmail.com <ryenguyen7411@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-10-07 01:49:38 +0000

    [Rye]~ Update some features.

--- G1/rye.nguyen/trunk/source/Game.cpp
+++ G1/rye.nguyen/trunk/source/Game.cpp
@@ -27,16 +27,23 @@ ErrorCode Game::Init(int screenW, int screenH, const char* title)
 	m_background->loadImage();
 	m_background->scale(0.5);
 
-	m_brick = new Brick* [45];
+	m_game_over = new Image("image\\gameover.png");
+	m_game_over->loadImage();
+
+	m_is_game_over = false;
+
+	m_count_of_brick = 45;
+	m_brick = new Brick*[m_count_of_brick];
 
 	int brick_width = 70;
 	int brick_height = 20;
 
-	for(int i = 0; i < 45; i++)
+	for(int i = 0; i < m_count_of_brick; i++)
 	{
 		m_brick[i] = new Brick();
 		m_brick[i]->Init(40 + (i % 9) * (brick_width + 10), 20 + (i / 9) * (brick_height + 10), brick_width, brick_height);
 	}
+	m_brick_active_left = m_count_of_brick;
 
 	m_bar = new Bar();
 	m_bar->Init();
@@ -49,67 +56,88 @@ ErrorCode Game::Init(int screenW, int screenH, const char* title)
 
 void Game::Update(float deltaTime)
 {
-	for(int i = 0; i < 45; i++)
+	if(!m_is_game_over)
 	{
-		if(m_brick[i]->IsAlive())
+		for(int i = 0; i < m_count_of_brick; i++)
 		{
-			bool collide_with_brick = m_ball->CheckCollisionWithObject(m_brick[i]->GetRect(), 0);
+			if(m_brick[i]->IsAlive())
+			{
+				bool collide_with_brick = m_ball->CheckCollisionWithObject(m_brick[i]->GetRect(), 0);
 
-			if(collide_with_brick)
-				m_brick[i]->DeActivate();
+				if(collide_with_brick)
+				{
+					m_brick[i]->DeActivate();
+					m_brick_active_left--;
+				}
+			}
+		}
+
+		for(int i = 0; i < m_count_of_brick; i++)
+		{
+			if(m_brick[i]->IsAlive())
+				m_brick[i]->Update();
+		}
+
+		if(m_brick_active_left == 0)
+			m_is_game_over = true;
+
+		if(getKeyState(KeyCode::KEY_LEFT) == KeyState::KEY_PRESSED)
+			m_bar->KeyPress(KeyCode::KEY_LEFT);
+		else if(getKeyState(KeyCode::KEY_RIGHT) == KeyState::KEY_PRESSED)
+			m_bar->KeyPress(KeyCode::KEY_RIGHT);
+		else
+			m_bar->KeyPress(KeyCode::KEY_UNKNOWN);
+
+		bool collide_with_bar = m_ball->CheckCollisionWithObject(m_bar->GetRect(), m_bar->GetVelocity());
+
+		if(!collide_with_bar)
+			m_bar->Update();
+
+		m_ball->Update();
+
+		if(IsExit())
+			Exit();
+
+		//Framerate Limited
+		if(1000.0f / FRAMERATE > deltaTime)
+		{
+			Sleep(1000.0f / FRAMERATE - deltaTime);
 		}
 	}
-
-	for(int i = 0; i < 45; i++)
-	{
-		if(m_brick[i]->IsAlive())
-			m_brick[i]->Update();
-	}
-
-	if(getKeyState(KeyCode::KEY_LEFT) == KeyState::KEY_PRESSED)
-		m_bar->KeyPress(KeyCode::KEY_LEFT);
-	else if(getKeyState(KeyCode::KEY_RIGHT) == KeyState::KEY_PRESSED)
-		m_bar->KeyPress(KeyCode::KEY_RIGHT);
-	else
-		m_bar->KeyPress(KeyCode::KEY_UNKNOWN);
-
-	bool collide_with_bar = m_ball->CheckCollisionWithObject(m_bar->GetRect(), m_bar->GetVelocity());
-
-	if(!collide_with_bar)
-		m_bar->Update();
-
-	m_ball->Update();
-
-	if(IsExit())
-		Exit();
-
-	//Framerate Limited
-	if(1000.0f / FRAMERATE > deltaTime)
-	{
-		Sleep(1000.0f / FRAMERATE - deltaTime);
-	}
+	else if(getKeyState(KeyCode::KEY_ENTER))
+		Reset();
 }
 
 void Game::Render(Graphics* g)
 {
 	g->cleanScreen();
 
-	g->drawImage(m_background, 0, 0);
-
-	for(int i = 0; i < 45; i++)
+	if(!m_is_game_over)
 	{
-		if(m_brick[i]->IsAlive())
-			m_brick[i]->Render(g);
-	}
+		g->drawImage(m_background, 0, 0);
 
-	m_bar->Render(g);
-	m_ball->Render(g);
+		for(int i = 0; i < 45; i++)
+		{
+			if(m_brick[i]->IsAlive())
+				m_brick[i]->Render(g);
+		}
+
+		m_bar->Render(g);
+		m_ball->Render(g);
+	}
+	else
+	{
+		g->drawImage(m_game_over, 0, 0);
+	}
 }
 
 void Game::Exit()
 {
 	m_background->unloadImage();
 	delete m_background;
+
+	m_game_over->unloadImage();
+	delete m_game_over;
 
 	m_ball->Release();
 	delete m_ball;
@@ -133,6 +161,20 @@ void Game::Exit()
 bool Game::IsExit()
 {
 	return getKeyState(KeyCode::KEY_ESCAPE) == KeyState::KEY_PRESSED;
+}
+
+void Game::Reset()
+{
+	for(int i = 0; i < 45; i++)
+	{
+		m_brick[i]->Activate();
+	}
+	m_brick_active_left = m_count_of_brick;
+
+	m_bar->Init();
+	m_ball->Init();
+
+	m_is_game_over = false;
 }
 
 /////////////////////////////////////
