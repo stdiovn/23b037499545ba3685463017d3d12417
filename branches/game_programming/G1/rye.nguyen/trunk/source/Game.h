9d19068418aabb287c091654ee05401b5c56a527
ryenguyen7411@gmail.com <ryenguyen7411@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-10-09 17:23:42 +0000

    [Rye]~ Upgrade some features

--- branches/game_programming/G1/rye.nguyen/trunk/source/Game.h
+++ branches/game_programming/G1/rye.nguyen/trunk/source/Game.h
@@ -18,13 +18,14 @@ private:
 	int					m_count_of_brick;
 	int					m_brick_active_left;
 
-	bool				m_is_game_over;
-	bool				m_is_win;
-	int					m_lives;
-
 	Image*				m_background;
 	Image*				m_game_over;
 	Image*				m_win;
+
+	bool				m_is_game_over;
+	bool				m_is_win;
+	int					m_lives;
+	bool				m_is_active;
 public:
 	Game();
 	virtual				~Game();
