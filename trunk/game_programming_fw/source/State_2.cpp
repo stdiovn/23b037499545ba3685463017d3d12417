662c4435d7eed94740578b1f531130c5cad57b52
uitdptai@gmail.com <uitdptai@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-10-21 06:30:29 +0000

    xóa những file không cần thiết

--- trunk/game_programming_fw/source/State_2.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "State_2.h"
-#define FileMap "data//Map_2.txt"
-State_2::State_2()
-{
-	BackGround::BackGround(FileMap);
-}
--- trunk/game_programming_fw/source/State_2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "BackGround.h"
-
-class State_2 : public BackGround
-{
-public:
-	State_2();
-};
