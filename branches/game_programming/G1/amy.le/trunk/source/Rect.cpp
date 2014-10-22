53da4b7259b77477e8ffa68e2a69e6fba2104d56
amyle.215@gmail.com <amyle.215@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-10-22 10:42:23 +0000

    +Update 5

--- branches/game_programming/G1/amy.le/trunk/source/Rect.cpp
+++ branches/game_programming/G1/amy.le/trunk/source/Rect.cpp
@@ -1,67 +1,148 @@
 #include "stdafx.h"
-#include "STDIO_FW\Video\Image.h"
 #include "STDIO_FW\Core\Application.h"
 #include "Rect.h"
-
-#define SRCWidth 800
-#define SRCHeight 600
-#define TenisNet "Net1.png"
-#define TenisNet2 "Net2.png"
-using namespace stdio_fw;
+#define SrcWidth 800
+#define SrcHeight 600
 
 Rect::Rect()
+{
+}
+void Rect::Init(int x, int y, int speedx, int speedy, int width, int height)
+{
+	m_X = x;
+	m_Y = y;
+	m_SpeedX = speedx;
+	m_SpeedY = speedy;
+	m_Width = width;
+	m_Height = height;
+	m_IsCollideRect = false;
+}
+int Rect::GetX()
+{
+	return m_X;
+}
+int Rect::GetY()
+{
+	return m_Y;
+}
+void Rect::SetX(int x)
+{
+	m_X = x;
+}
+void Rect::SetY(int y)
+{
+	m_Y = y;
+}
+int Rect::GetSpeedX()
+{
+	return m_SpeedX;
+}
+int Rect::GetSpeedY()
+{
+	return m_SpeedY;
+}
+void Rect::SetSpeedX(int speedx)
+{
+	m_SpeedX = speedx;
+}
+void Rect::SetSpeedY(int speedy)
+{
+	m_SpeedY = speedy;
+}
+void Rect::TransferOfControlledRect(KeyState KeyLeft, KeyState KeyRight)
+{
+	if ((KeyRight == 1) && (m_X + m_Width < SrcWidth))
+	{
+		m_X += m_SpeedX;
+		if (m_X > SrcWidth - m_Width)
+		{
+			m_X = SrcWidth - m_Width;
+		}
+	}
+	else if ((KeyLeft == 1) && (m_X > 0))
+	{
+		m_X -= m_SpeedX;
+		if (m_X < 0)
+		{
+			m_X = 0;
+		}
+	}
+}
+void Rect::TransferOfAutoRect()
+{
+	m_X += m_SpeedX;
+	m_Y += m_SpeedY;
+
+	if (m_X >= SrcWidth - m_Width)
+	{
+		m_X = SrcWidth - m_Width;
+		m_SpeedX = -m_SpeedX;
+	}
+	else if (m_X <= 0)
+	{
+		m_X = 0;
+		m_SpeedX = -m_SpeedX;
+	}
+
+	if (m_Y >= SrcHeight - m_Height)
+	{
+		m_Y = SrcHeight - m_Height;
+		m_SpeedY = -m_SpeedY;
+	}
+	else if (m_Y <= 0)
+	{
+		m_Y = 0;
+		m_SpeedY = -m_SpeedY;
+	}
+}
+
+int Rect::GetIsCollide()
+{
+	return m_IsCollideRect;
+}
+void Rect::CollideWithRect(Rect* R)
+{
+	m_IsCollideRect = false;
+	if ((m_X + m_Width >= R->GetX()) && (m_X <= R->GetX() + m_Width))
+	{
+		if ((m_Y + m_Width > R->GetY()) && (m_Y + m_Width < R->GetY() + SrcHeight) && (m_SpeedY > 0))
+		{
+			m_Y = R->GetY() - m_Width;
+		}
+		else if ((m_Y < R->GetY() + SrcHeight) && (m_Y > R->GetY()) && (m_SpeedY < 0))
+		{
+			m_Y = R->GetY() + SrcHeight;
+		}
+		if ((m_Y + m_Width == R->GetY()) || (m_Y == R->GetY() + SrcHeight))
+		{
+			m_SpeedY = -m_SpeedY;
+			m_IsCollideRect = true;
+		}
+	}
+
+	if ((m_Y + m_Width >= R->GetY()) && (m_Y <= R->GetY() + SrcHeight))
+	{
+		if ((m_X + m_Width > R->GetX()) && (m_X < R->GetX()) && (m_SpeedX > 0))
+		{
+			m_X = R->GetX() - m_Width;
+		}
+		else if ((m_X < R->GetX() + SrcWidth) && (m_X > R->GetX() + SrcWidth - m_Width) && (m_SpeedX < 0))
+		{
+			m_X = R->GetX() + SrcWidth;
+		}
+		if ((m_X + m_Width == R->GetX()) || (m_X == R->GetX() + SrcWidth))
+		{
+			m_SpeedX = -m_SpeedX;
+			m_IsCollideRect = true;
+		}
+	}
+}
+
+void Rect::Release()
 {
 
 }
 
 Rect::~Rect()
 {
-
-}
-
-void Rect::InitRect()
-{
-	m_RectSpeedX = 10;
-	m_RectX = 0;
-	m_RectY = 500;
-	m_RectImage = new Image(TenisNet);
-	m_RectImage->loadImage();
-}
-
-int Rect::GetRectX()
-{
-	return m_RectX;
-}
-
-int Rect::GetRectY()
-{
-	return m_RectY;
-}
-
-Image* Rect::GetRectImage()
-{
-	return m_RectImage;
-}
-
-void Rect::ConllideRect()
-{
-	
-}
-
-void Rect::TransferRect(KeyState KeyRight, KeyState KeyLeft)
-{
-	if ((KeyRight == 1) && (m_RectX + RectWidth <SRCWidth))
-	{
-		m_RectX += m_RectSpeedX;
-	}
-	else
-	if ((KeyLeft == 1) && (m_RectX > 0))
-	{
-		m_RectX -= m_RectSpeedX;
-	}
-}
-
-void Rect::ReleaseRect()
-{
-	m_RectImage->~Image();
 }
--- branches/game_programming/G1/amy.le/trunk/source/Rect.h
+++ branches/game_programming/G1/amy.le/trunk/source/Rect.h
@@ -1,26 +1,34 @@
-#ifndef __RECT_H__
-#define __RECT_H__
-
-#define RectWidth 60
-#define  RectHeight 90
+#pragma once
+#include "stdafx.h"
+#include "STDIO_FW\Core\Application.h"
+#include "STDIO_FW\Video\Graphics.h"
 using namespace stdio_fw;
 class Rect
 {
 private:
-	int m_RectSpeedX;
-	int m_RectX;
-	int m_RectY;
-	Image* m_RectImage;
+	int m_X;
+	int m_Y;
+	int m_SpeedX;
+	int m_SpeedY;
+	int m_Width;
+	int m_Height;
+	int m_IsCollideRect;
 public:
 	Rect();
+	void Init(int x, int y, int speedx, int speedy, int width, int height);
+	int GetX();
+	int GetY();
+	void SetX(int x);
+	void SetY(int y);
+	int GetSpeedX();
+	int GetSpeedY();
+	int GetIsCollide();
+	void SetSpeedX(int speedx);
+	void SetSpeedY(int speedy);
+	void TransferOfControlledRect(KeyState KeyLeft, KeyState KeyRight);
+	void TransferOfAutoRect();
+	void CollideWithRect(Rect* R);
+	void Release();
 	~Rect();
-	void InitRect();
-	int GetRectX();
-	int GetRectY();
-	Image* GetRectImage();
-	void ConllideRect();
-	void TransferRect(KeyState KeyRight, KeyState KeyLeft);
-	void ReleaseRect();
 };
 
-#endif
