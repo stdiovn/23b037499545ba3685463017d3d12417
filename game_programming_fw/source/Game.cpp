e4ce0554e53d39b51d3133a7471482b5c8f0c827
vquanghuy@gmail.com <vquanghuy@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-11-01 03:29:04 +0000

    ! revert change make by Amy and Tai

--- game_programming_fw/source/Game.cpp
+++ game_programming_fw/source/Game.cpp
@@ -4,24 +4,6 @@
 #include "STDIO_FW\Video\Graphics.h"
 
 #include "Game.h"
-#include "Map1.h"
-#include "Map2.h"
-#define FPS 30
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-#include "Ball.h"
-#include "ThanhMove.h"
-
-#define MapFile2 "data//Map_2.txt"
-#define MapFile "data//Map_1.txt"
-
-
-MapObject *state = NULL;
-
-MapObject *state1 = NULL;
-MapObject *state2 = NULL;
-Ball *ball = NULL;
-ThanhMove *Space = NULL;
 
 Game::Game()
 {
@@ -30,94 +12,26 @@ Game::Game()
 
 Game::~Game()
 {
-	ball->Clear();
-	state1->Clear();
-	state2->Clear();
-	Space->Clear();
+
 }
 
 ErrorCode Game::Init(int screenW, int screenH, const char* title)
 {
 	ErrorCode errCode = Application::Init(screenW, screenH, title);
 
-	ball = new Ball();
-	ball->setPosition(100, 300);
-
-	state1 = new Map1();
-	state1->InitMap(MapFile);
-
-	state2 = new Map2();
-	state2->InitMap(MapFile2);
-
-	state = state1;
-
-	Space = new ThanhMove(100, 450);
-
 	return errCode;
 }
 
 void Game::Update(float deltaTime)
 {
-	// set gamerun with time 1000 / fps (ms)
-	float lasttime = GetTickCount();
-	float current;
-	do
-	{
-		current = GetTickCount();
-	} while (current - lasttime + deltaTime < (float)1000 / FPS);
 
-	if (state->NextMap())
-	{
-		state = state1;
-	}
-
-	if (ball->UpdateCollisionWindows(SCREEN_WIDTH, SCREEN_HEIGHT) == DIR::ABOVE)
-	{
-		state->setIsActive(false);
-	}
-
-	Space->UpdateCollisionWithWindows(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	for (int i = 0; i < state->getCountFrameWallOnMap(); i++)
-	{
-		Object temp = state->getPosition(i);
-		if (state->isActiveRenderAt(i) && ball->isCollsionObject(temp.X, temp.X + state->getWidth(i), temp.Y, temp.Y + state->getHeight(i)))
-		{
-			ball->UpdateVeclocity(temp.X, temp.X + state->getWidth(i), temp.Y, temp.Y + state->getHeight(i));
-			state->deActiveRenderAt(i);
-			break;
-		}
-	}
-
-	if (ball->isCollsionObject(Space->getPositionX(), Space->getPositionX() + Space->getWidth(), Space->getPositionY(), Space->getPositionY() + Space->getHeight()))
-	{
-		ball->UpdateVeclocity(Space->getPositionX(), Space->getPositionX() + Space->getWidth(), Space->getPositionY(), Space->getPositionY() + Space->getHeight());
-	}
-
-	if (getKeyState(KEY_LEFT) == KeyState::KEY_PRESSED)
-	{
-		Space->UpdateSpeedKeyBoard(KEY_LEFT);
-	}
-	else if (getKeyState(KEY_RIGHT) == KeyState::KEY_PRESSED)
-	{
-		Space->UpdateSpeedKeyBoard(KEY_RIGHT);
-	}
-
-	ball->Update();
 }
 
 void Game::Render(Graphics* g)
 {
 	g->cleanScreen();
 	g->setColor(0x00FF00FF);
-	
-	state->Render(g);
-
-	if (state->isGameOver()) return;
-
-	ball->Render(g);
-	Space->Render(g);
-
+	g->drawRect(50, 50, 200, 200);
 }
 
 void Game::Exit()
@@ -131,7 +45,7 @@ void Game::Exit()
 void main()
 {
 	Game g;
-	g.Init(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
+	g.Init(800, 600, "Game");
 
 	g.Run();
 }
