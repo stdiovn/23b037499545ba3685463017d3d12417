a650a9ea80e4afe8c174df0a5bb477ed89b425a9
tmcuong93@gmail.com <tmcuong93@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2014-12-25 14:23:31 +0000

    Version1

---
+++ TB2002/cuong.tranminh/source/Ball.cpp
@@ -0,0 +1,243 @@
+﻿#include "stdafx.h"
+#include "Ball.h"
+
+
+Ball::Ball()
+{
+	dBall = ES;
+	rDynamicObject.x = 460;
+	rDynamicObject.y = 600 - 60;
+}
+
+
+Ball::~Ball()
+{
+	
+}
+
+void Ball::Init()
+{
+	im = new Image(BALL);
+	im->loadImage();
+	//rDynamicObject.x = 35; rDynamicObject.y = 100;
+	rDynamicObject.height = rDynamicObject.width = 25;
+}
+//void Ball::Update()
+//{
+//	Sleep(1);
+//	if (isRight == NULL && isUP == NULL)
+//	{
+//		isRight = false; isUP = true;
+//		rDynamicObject.x--;
+//		rDynamicObject.y++;
+//	}
+//	//Bóng hướng lên
+//	if (isUP)
+//	{
+//		if (isRight)
+//		{
+//			
+//			if (rDynamicObject.y == 0)
+//			{
+//				isUP = false;
+//				isRight = true;
+//				rDynamicObject.x++;
+//				rDynamicObject.y++;
+//				return;
+//			}
+//			if (rDynamicObject.x == 800 - 25)
+//			{
+//				isUP = true;
+//				isRight = false;
+//				rDynamicObject.y--;
+//				rDynamicObject.x--;
+//				return;
+//			}
+//			rDynamicObject.x++;
+//		}
+//		if (!isRight)
+//		{
+//			
+//			if (rDynamicObject.x == 0)
+//			{
+//				isUP = isRight = true;
+//				rDynamicObject.x++;
+//				rDynamicObject.y--;
+//				return;
+//			}
+//			if (rDynamicObject.y == 0)
+//			{
+//				isUP = isRight = false;
+//				rDynamicObject.x--;
+//				rDynamicObject.y++;
+//				return;
+//			}
+//			rDynamicObject.x--;
+//		}
+//
+//		rDynamicObject.y--;
+//	}
+//
+//	//Bóng hướng xuống
+//	if (!isUP)
+//	{
+//		
+//		if (isRight)
+//		{
+//			
+//			if (rDynamicObject.x == 800 - 25)
+//			{
+//				isUP = isRight = false;
+//				rDynamicObject.x--;
+//				rDynamicObject.y++;
+//				return;
+//			}
+//			if (rDynamicObject.y == 600 - 25)
+//			{
+//				isRight = isUP = true;
+//
+//				rDynamicObject.x++;
+//				rDynamicObject.y--;
+//				return;
+//			}
+//			rDynamicObject.x++;
+//		}
+//		if (!isRight)
+//		{
+//			
+//			if (rDynamicObject.x == 0)
+//			{
+//				isRight = true;
+//				isUP = false;
+//				rDynamicObject.x++;
+//				rDynamicObject.y++;
+//				return;
+//			}
+//			if (rDynamicObject.y == 600 - 25)
+//			{
+//				isUP = true;
+//				isRight = false;
+//				rDynamicObject.x--;
+//				rDynamicObject.y--;
+//				return;
+//			}
+//			rDynamicObject.x--;
+//		}
+//		rDynamicObject.y++;
+//	}
+//	
+//}
+void Ball::Render(Graphics* g)
+{
+	g->drawImage(im, rDynamicObject, 0);
+}
+
+void Ball::Update()
+{
+	Sleep(1);
+	if (dBall == ES)
+	{
+		if (rDynamicObject.x == 775)
+		{
+			dBall = WS;
+			rDynamicObject.x--;
+			rDynamicObject.y++;
+			return;
+		}
+		if (rDynamicObject.y == 575)
+		{
+			dBall = EN;
+			rDynamicObject.y--;
+			rDynamicObject.x++;
+			return;
+		}
+		rDynamicObject.x++;
+		rDynamicObject.y++;
+	}
+	if (dBall == EN)
+	{
+		if (rDynamicObject.y == 0)
+		{
+			dBall = ES;
+			rDynamicObject.x++;
+			rDynamicObject.y++;
+			return;
+		}
+		if (rDynamicObject.x == 775)
+		{
+			dBall = WN;
+			rDynamicObject.x--;
+			rDynamicObject.y--;
+			return;
+		}
+		rDynamicObject.x++;
+		rDynamicObject.y--;
+	}
+	if (dBall == WN)
+	{
+		if (rDynamicObject.x == 0)
+		{
+			dBall = EN;
+			rDynamicObject.x++;
+			rDynamicObject.y--;
+			return;
+		}
+
+		if (rDynamicObject.y == 0)
+		{
+			dBall = WS;
+			rDynamicObject.x--;
+			rDynamicObject.y++;
+			return;
+		}
+		rDynamicObject.x--;
+		rDynamicObject.y--;
+	}
+	if (dBall == WS)
+	{
+		if (rDynamicObject.x == 0)
+		{
+			dBall = ES;
+			rDynamicObject.x++;
+			rDynamicObject.y++;
+			return;
+		}
+		if (rDynamicObject.y == 575)
+		{
+			dBall = WN;
+			rDynamicObject.x--;
+			rDynamicObject.y--;
+		}
+		rDynamicObject.x--;
+		rDynamicObject.y++;
+	}
+}
+void Ball::setDirection(DIRECTION d)
+{
+	dBall = d;
+}
+
+DIRECTION Ball::getDirection()
+{
+	if (dBall == ES)
+	{
+		dBall = EN;
+		return dBall;
+	}
+	if (dBall == WS)
+	{
+		dBall = WN;
+		return dBall;
+	}
+	if (dBall == EN)
+	{
+		dBall = ES;
+		return dBall;
+	}
+	if (dBall == WN)
+	{
+		dBall = WS;
+		return dBall;
+	}
+
+}
--- TB2002/cuong.tranminh/source/Game.cpp
+++ TB2002/cuong.tranminh/source/Game.cpp
@@ -1,35 +1,70 @@
 #include "stdafx.h"
-
 #include "Game.h"
+
+Brick br;
+
 
 Game::Game()
 {
 
 }
-
 Game::~Game()
 {
-
+	
 }
 
-ErrorCode Game::Init(int screenW, int screenH, const char* title)
+ErrorCode Game::init(int screenW, int screenH, const char* title)
 {
-	ErrorCode errCode = Application::Init(screenW, screenH, title);	
+	ErrorCode errCode = Application::init(screenW, screenH, title);	
+	ball.Init();
+	bar.Init();
+	inputArr(ArrBrick);
+	for (int i = 0; i < ArrBrick.size(); i++)ArrBrick[i].Init();
 
 	return errCode;
 }
 
-void Game::Update(float deltaTime)
+void Game::update(float deltaTime)
 {
+	if (getKeyState(KEY_LEFT))
+	{
+		bar.moveleft();
+		if (Collision(ball.getRect(), bar.getRect()) || Collision(bar.getRect(), ball.getRect()))ball.setDirection(WN);
+	}
+	if (getKeyState(KEY_RIGHT))
+	{
+		bar.moveright();
+		if (Collision(ball.getRect(), bar.getRect()) || Collision(bar.getRect(), ball.getRect()))ball.setDirection(EN);
+	}
+	if (Collision(ball.getRect(), bar.getRect()) || Collision(bar.getRect(), ball.getRect()))ball.setDirection(ball.getDirection());
+	for each(Brick brick in ArrBrick)
+	{
+		if (Collision(ball.getRect(), brick.getRect()) && br.getalive())
+		{
 	
+			ball.setalive(ball.getDirection());
+			brick.setalive(false);
+		}
+	}
+	
+	ball.Update();
 }
 
-void Game::Render(Graphics* g)
+void Game::render(Graphics* g)
 {
 	g->cleanScreen();	
+	ball.Render(g);
+	bar.Render(g);
+	
+	for each(Brick b in ArrBrick)b.Render(g);
 }
 
-void Game::Exit()
+void Game::exit()
 {
 	
+}
+
+void Game::onKeyProc(KeyCode key, KeyState state)
+{
+
 }
