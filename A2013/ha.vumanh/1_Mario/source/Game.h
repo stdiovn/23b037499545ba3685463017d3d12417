22010aec77e48dfc89746184473aefdde6ebcb6b
vquanghuy@gmail.com <vquanghuy@gmail.com@10ccfff5-0609-2bc2-5f32-f7b7f322fe12>
2015-01-23 15:02:07 +0000

    - [A2013] re-arrange project structure

---
+++ A2013/ha.vumanh/1_Mario/source/Game.h
@@ -0,0 +1,17 @@
+#pragma once
+
+using namespace stdio_fw;
+class Game : public Application
+{
+public:
+	Game();
+	virtual ~Game();
+
+	virtual ErrorCode	init(int screenW, int screenH, const char* title);
+	virtual void		update(float deltaTime);
+
+	virtual void		render(Graphics* g);
+	virtual void		exit();
+
+	virtual void		onKeyProc(KeyCode key, KeyState state);
+};
